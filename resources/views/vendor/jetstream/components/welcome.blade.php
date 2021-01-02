1e39362d90bc99f18a182f6fe4ad73f10928d091
stelladum <stella.dumencic@gmail.com>
2021-01-02 12:11:01 +0100

    designed dashboard

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -2,19 +2,13 @@
     <div class="col-md-12">
         <div class="card shadow bg-light">
             <div class="card-body bg-white px-5 py-3 border-bottom rounded-top">
-                <div>
-                    <x-jet-application-logo style="width: 317px;" />
-                </div>
 
                 <h3 class="h3 my-4">
-                    Welcome to your Jetstream application!
+                    Welcome to your profile!
                 </h3>
 
                 <div class="text-muted">
-                    Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                    to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                    you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                    ecosystem to be a breath of fresh air. We hope you love it.
+                    Lorem ipsum
                 </div>
             </div>
             <div class="row g-0">
@@ -26,20 +20,11 @@
                             </div>
                             <div class="pl-3">
                                 <div class="mb-2">
-                                    <a href="https://laravel.com/docs" class="h5 font-weight-bolder text-decoration-none text-dark">Documentation</a>
+                                    <p class="h5 font-weight-bolder text-decoration-none text-dark">Reservations</p>
                                 </div>
                                 <p class="text-muted">
-                                    Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
+                                    Lorem ipsum
                                 </p>
-                                <a href="https://laravel.com/docs" class="text-decoration-none">
-                                    <div class="mt-3 d-flex align-content-center font-weight-bold text-primary">
-                                        <div>Explore the documentation</div>
-
-                                        <div class="ml-1 text-primary">
-                                            <svg viewBox="0 0 20 20" fill="currentColor" width="16" class="arrow-right w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                        </div>
-                                    </div>
-                                </a>
                             </div>
                         </div>
                     </div>
@@ -53,20 +38,11 @@
                             </div>
                             <div class="pl-3">
                                 <div class="mb-2">
-                                    <a href="https://laracasts.com" class="h5 font-weight-bolder text-decoration-none text-dark">Laracasts</a>
+                                    <p class="h5 font-weight-bolder text-decoration-none text-dark">Profile</p>
                                 </div>
                                 <p class="text-muted">
-                                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                    Lorem ipsum
                                 </p>
-                                <a href="https://laravel.com/docs" class="text-decoration-none">
-                                    <div class="mt-3 d-flex align-content-center font-weight-bold text-primary">
-                                        <div>Start watching Laracasts</div>
-
-                                        <div class="ml-1 text-primary">
-                                            <svg viewBox="0 0 20 20" fill="currentColor" width="16" class="arrow-right w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                        </div>
-                                    </div>
-                                </a>
                             </div>
                         </div>
                     </div>
@@ -80,10 +56,10 @@
                             </div>
                             <div class="pl-3">
                                 <div class="mb-2">
-                                    <a href="https://getbootstrap.com/" class="h5 font-weight-bolder text-decoration-none text-dark">Bootstrap</a>
+                                    <p class="h5 font-weight-bolder text-decoration-none text-dark">See testimonials</p>
                                 </div>
                                 <p class="text-muted">
-                                    Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.
+                                    Lorem ipsum
                                 </p>
                             </div>
                         </div>
@@ -98,10 +74,10 @@
                             </div>
                             <div class="pl-3">
                                 <div class="mb-2">
-                                    <span class="h5 font-weight-bolder text-decoration-none text-dark">Authentication</span>
+                                    <p class="h5 font-weight-bolder text-decoration-none text-dark">Log out</p>
                                 </div>
                                 <p class="text-muted">
-                                    Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started what matters most: building your application.
+                                    Lorem ipsum
                                 </p>
                             </div>
                         </div>
