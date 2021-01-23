ec9604c5272aad5cb7abdd7c95c23f541ade158c
stelladum <stella.dumencic@gmail.com>
2021-01-23 14:20:27 +0100

    user history responsivness

--- resources/views/userhistory.blade.php
+++ resources/views/userhistory.blade.php
@@ -1,20 +1,26 @@
 <x-app-layout>
     @push('scripts')
-    <script>
-        $(document).ready(function(){
-          $("#myInput").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $("#myTable tr").filter(function() {
-              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        <script>
+            $(document).ready(function() {
+                $("#myInput").on("keyup", function() {
+                    var value = $(this).val().toLowerCase();
+                    $("#myTable tr").filter(function() {
+                        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                    });
+                });
             });
-          });
-        });
-    </script>
-    <script>
-        function sortTable(columnId, descending) {
-            $("#UserList").tablesorter({ sortList: [[columnId,descending]] });
-        }
-    </script>
+
+        </script>
+        <script>
+            function sortTable(columnId, descending) {
+                $("#UserList").tablesorter({
+                    sortList: [
+                        [columnId, descending]
+                    ]
+                });
+            }
+
+        </script>
     @endpush
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
@@ -26,28 +32,31 @@
 
                 <input class="form-control | w-25 p-3" id="myInput" type="text" placeholder="Search..">
 
-                <table class="table table-striped" id="UserList">
-                    <thead>
-                        <tr>
-                            <th style="cursor: pointer;" scope="col" onClick="sortTable(0, 0)">Name of user and package</th>
-                            <th style="cursor: pointer;" scope="col" onClick="sortTable(1, 0)">Start</th>
-                            <th style="cursor: pointer;" scope="col" onClick="sortTable(2, 0)">End</th>
-                            <th style="cursor: pointer;" scope="col" onClick="sortTable(3, 0)">Color</th>
-                            <th style="cursor: pointer;" scope="col" onClick="sortTable(4, 0)">Text Color</th>
-                        </tr>
-                    </thead>
-                    <tbody id="myTable">
-                        @foreach ($events as $event)
+                <div class="table-responsive">
+                    <table class="table table-striped" id="UserList">
+                        <thead>
                             <tr>
-                                <td>{{ $event->user->name }} - {{ $event->title }}</td>
-                                <td>{{ $event->start }}</td>
-                                <td>{{ $event->end }}</td>
-                                <td class="color" style="background: {{ $event->color }}"></td>
-                                <td class="color" style="background: {{ $event->textColor }}"></td>
+                                <th style="cursor: pointer;" scope="col" onClick="sortTable(0, 0)">Name of user and
+                                    package <span style="color: rgb(0, 168, 243);">▲▼</span></th>
+                                <th style="cursor: pointer;" scope="col" onClick="sortTable(1, 0)">Start <span style="color: rgb(0, 168, 243);">▲▼</span></th>
+                                <th style="cursor: pointer;" scope="col" onClick="sortTable(2, 0)">End <span style="color: rgb(0, 168, 243);">▲▼</span></th>
+                                <th style="cursor: pointer;" scope="col" onClick="sortTable(3, 0)">Color</th>
+                                <th style="cursor: pointer;" scope="col" onClick="sortTable(4, 0)">Text Color</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody id="myTable">
+                            @foreach ($events as $event)
+                                <tr>
+                                    <td>{{ $event->user->name }} - {{ $event->title }}</td>
+                                    <td>{{ $event->start }}</td>
+                                    <td>{{ $event->end }}</td>
+                                    <td class="color" style="background: {{ $event->color }}"></td>
+                                    <td class="color" style="background: {{ $event->textColor }}"></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
